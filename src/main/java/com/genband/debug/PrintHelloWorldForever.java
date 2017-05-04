fe4a7ac75dd8a86b3e12a755aefa57dd263f3ed7
Sen Wang <Sen.Wang@kandy.io>
2017-05-04 16:40:05 -0400

    so called everything

--- src/main/java/com/genband/debug/PrintHelloWorldForever.java
+++ src/main/java/com/genband/debug/PrintHelloWorldForever.java
@@ -7,6 +7,7 @@ public class PrintHelloWorldForever {
             try {
                 Thread.sleep(3000);
                 System.out.println("Hello World!!!");
+                System.out.println("Hahaha the debug: " + args);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
